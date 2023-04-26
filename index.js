513f7886b1df5820b0209d14aff6f1193258c584
sahil <sahilvichare81@gmail.com>
2023-04-26 20:36:39 +0530

    3

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app = express()
 const authroutes = require('./routes/routes')
+const cors = require("cors")
 
 
 app.use(express.json())
@@ -9,7 +10,13 @@ require('./modal/userSchema')
 
 app.use('/', authroutes)
 
+app.options("*", cors())
 
+const corsOptions = {
+  origin:"http://localhost:3000/"
+}
+
+app.use(cors(corsOptions))
 
 app.listen(5000 , ()=> {
     console.log('Server is running on port 5000')
